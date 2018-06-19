0daf9c851d32720555cfb71d0a205b5486dfd061
Stefan Thomas <justmoon@members.fsf.org>
2018-06-19 13:59:06 +0200

    feat: output geojson as a FeatureCollection

--- to-geo-json.js
+++ to-geo-json.js
@@ -3,16 +3,19 @@
 const hosts = require('./hosts.json')
 const { writeFileSync } = require('fs')
 
-const geo = hosts.map(h => ({
-  type: 'Feature',
-  properties: {
-    name: h.host,
-    ip: h.ip
-  },
-  geometry: {
-    type: 'Point',
-    coordinates: [ h.geo.ll[1], h.geo.ll[0] ]
-  }
-}))
+const geo = {
+  type: 'FeatureCollection',
+  features: hosts.map(h => ({
+    type: 'Feature',
+    properties: {
+      name: h.host,
+      ip: h.ip
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [ h.geo.ll[1], h.geo.ll[0] ]
+    }
+  }))
+}
 
 writeFileSync('hosts.geo.json', JSON.stringify(geo, null, 2))
