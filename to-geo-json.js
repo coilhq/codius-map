3ce6ac958936c7bddbcbb989ec5ca4b6444b1642
Stefan Thomas <justmoon@members.fsf.org>
2018-06-19 13:54:27 +0200

    feat: generate GeoJSON data

---
+++ to-geo-json.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const hosts = require('./hosts.json')
+const { writeFileSync } = require('fs')
+
+const geo = hosts.map(h => ({
+  type: 'Feature',
+  properties: {
+    name: h.host,
+    ip: h.ip
+  },
+  geometry: {
+    type: 'Point',
+    coordinates: [ h.geo.ll[1], h.geo.ll[0] ]
+  }
+}))
+
+writeFileSync('hosts.geo.json', JSON.stringify(geo, null, 2))
